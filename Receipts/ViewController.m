ebdf9c3b27cc83329d3afa4dcadffaf8d9741173
olga-nest <olga.w.nesterova@gmail.com>
2017-10-26 22:15:00 -0700

    Get unique tags

--- Receipts/ViewController.m
+++ Receipts/ViewController.m
@@ -14,14 +14,23 @@
 
 @property (nonatomic, strong) NSArray<Receipt *> *receipts;
 @property (nonatomic, strong) NSArray<Tag *> *tags;
+@property (nonatomic, strong) NSArray *uniqueTags;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
+    
+    
     [super viewDidLoad];
     [self getAllReceipts];
     [self getAllTags];
+    [self getUniqueTags];
+    
+    
+
+    
+    
 }
 
 -(void)viewWillAppear:(BOOL)animated {
@@ -76,9 +85,9 @@
 
 #pragma mark - Table View
 // TODO!!! from fetched results
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return self.tags.count;
-}
+//- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+//  return self.tagsSet.count;
+//}
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     return self.receipts.count;
@@ -88,6 +97,7 @@
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cellId" forIndexPath:indexPath];
     Receipt *receipt = [self.receipts objectAtIndex:indexPath.row];
+//    NSArray *tags = receipt.tag.allObjects;
     cell.textLabel.text = receipt.note;
     cell.detailTextLabel.text = [NSString stringWithFormat:@"%f", receipt.amount];
 
@@ -123,7 +133,7 @@
 }
 
 
-#pragma mark - Fetched results 332
+#pragma mark - Fetched results
 
 -(void)getAllReceipts {
     AppDelegate *appDelegate = (AppDelegate *)[UIApplication sharedApplication].delegate;
@@ -141,97 +151,20 @@
     NSManagedObjectContext *context = persistentContainer.viewContext;
     NSFetchRequest *request = [Tag fetchRequest];
     self.tags = [context executeFetchRequest:request error:nil];
+    NSLog(@"There are %lu objects in tags array", self.tags.count);
 
 }
 
-//- (NSFetchedResultsController<Receipt *> *)fetchedResultsController {
-//    if (_fetchedResultsController != nil) {
-//        return _fetchedResultsController;
-//    }
-//    
-//    NSFetchRequest<Receipt *> *fetchRequest = Receipt.fetchRequest;
-//    
-//    // Set the batch size to a suitable number.
-//    [fetchRequest setFetchBatchSize:20];
-//    
-//    // Edit the sort key as appropriate.
-//    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"tag" ascending:NO];
-//    
-//    [fetchRequest setSortDescriptors:@[sortDescriptor]];
-//    
-//    // Edit the section name key path and cache name if appropriate.
-//    // nil for section name key path means "no sections".
-//    NSFetchedResultsController<Receipt *> *aFetchedResultsController = [[NSFetchedResultsController alloc] initWithFetchRequest:fetchRequest managedObjectContext:self.managedObjectContext sectionNameKeyPath:nil cacheName:@"Master"];
-//    aFetchedResultsController.delegate = self;
-//    
-//    NSError *error = nil;
-//    if (![aFetchedResultsController performFetch:&error]) {
-//        // Replace this implementation with code to handle the error appropriately.
-//        // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-//        NSLog(@"Unresolved error %@, %@", error, error.userInfo);
-//        abort();
-//    }
-//    
-//    _fetchedResultsController = aFetchedResultsController;
-//    return _fetchedResultsController;
-//}
-//
-//- (void)controllerWillChangeContent:(NSFetchedResultsController *)controller {
-//    [self.tableView beginUpdates];
-//}
-//
-//- (void)controller:(NSFetchedResultsController *)controller didChangeSection:(id <NSFetchedResultsSectionInfo>)sectionInfo
-//           atIndex:(NSUInteger)sectionIndex forChangeType:(NSFetchedResultsChangeType)type {
-//    switch(type) {
-//        case NSFetchedResultsChangeInsert:
-//            [self.tableView insertSections:[NSIndexSet indexSetWithIndex:sectionIndex] withRowAnimation:UITableViewRowAnimationFade];
-//            break;
-//            
-//        case NSFetchedResultsChangeDelete:
-//            [self.tableView deleteSections:[NSIndexSet indexSetWithIndex:sectionIndex] withRowAnimation:UITableViewRowAnimationFade];
-//            break;
-//            
-//        default:
-//            return;
-//    }
-//}
-//
-//- (void)controller:(NSFetchedResultsController *)controller didChangeObject:(id)anObject
-//       atIndexPath:(NSIndexPath *)indexPath forChangeType:(NSFetchedResultsChangeType)type
-//      newIndexPath:(NSIndexPath *)newIndexPath {
-//    UITableView *tableView = self.tableView;
-//    
-//    switch(type) {
-//        case NSFetchedResultsChangeInsert:
-//            [tableView insertRowsAtIndexPaths:@[newIndexPath] withRowAnimation:UITableViewRowAnimationFade];
-//            break;
-//            
-//        case NSFetchedResultsChangeDelete:
-//            [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-//            break;
-//            
-//        case NSFetchedResultsChangeUpdate:
-//            [self configureCell:[tableView cellForRowAtIndexPath:indexPath] withEvent:anObject];
-//            break;
-//            
-//        case NSFetchedResultsChangeMove:
-//            [self configureCell:[tableView cellForRowAtIndexPath:indexPath] withEvent:anObject];
-//            [tableView moveRowAtIndexPath:indexPath toIndexPath:newIndexPath];
-//            break;
-//    }
-//}
-//
-//- (void)controllerDidChangeContent:(NSFetchedResultsController *)controller {
-//    [self.tableView endUpdates];
-//}
-//
-///*
-// // Implementing the above methods to update the table view in response to individual changes may have performance implications if a large number of changes are made simultaneously. If this proves to be an issue, you can instead just implement controllerDidChangeContent: which notifies the delegate that all section and object changes have been processed.
-// 
-// - (void)controllerDidChangeContent:(NSFetchedResultsController *)controller {
-// // In the simplest, most efficient, case, reload the table view.
-// [self.tableView reloadData];
-// }
-// */
-//
+-(void)getUniqueTags {
+    if (!self.uniqueTags) {
+        self.uniqueTags = [NSArray new];
+    }
+    
+    NSArray *tagName = [self.tags valueForKey:@"tagName"];
+    NSLog(@"%@", tagName);
+    
+    NSArray *uniqueTags = [tagName valueForKeyPath:@"@distinctUnionOfObjects.self"];
+    NSLog(@"uniqueTags: %@", uniqueTags);
+    }
+
 @end
