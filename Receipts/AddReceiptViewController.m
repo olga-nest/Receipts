6f28835d1f98923875e5c45982c92794ce31ce11
olga-nest <olga.w.nesterova@gmail.com>
2017-10-26 18:24:11 -0700

    Save tags for receipt

--- Receipts/AddReceiptViewController.m
+++ Receipts/AddReceiptViewController.m
@@ -17,12 +17,26 @@
 @property (weak, nonatomic) IBOutlet UITextField *noteField;
 @property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
 
+@property (nonatomic) NSMutableSet *tags;
+
 @end
 
 @implementation AddReceiptViewController
 
-- (IBAction)addTag:(id)sender {
+- (IBAction)addTag:(UIButton *)sender {
+//    NSMutableSet *tempSet = [NSMutableSet new];
+    
+    AppDelegate *appDelegate = (AppDelegate *)[UIApplication sharedApplication].delegate;
+    NSPersistentContainer *persistentContainer = appDelegate.persistentContainer;
+    
+    Tag *tag = [[Tag alloc]initWithContext:persistentContainer.viewContext];
+    tag.tagName = sender.titleLabel.text;
+    NSLog(@"Adding %@ tag to tag set", tag.tagName);
+    
+    [self.tags addObject:tag];
+//    self.tags = tempSet;
 }
+
 - (IBAction)cancel:(id)sender {
     [self dismissViewControllerAnimated:YES completion:nil];
 }
@@ -36,23 +50,29 @@
     receipt.amount = [self.amountField.text floatValue];
     NSLog(@"Saving %f amount", receipt.amount);
     
+    receipt.timeStamp = [self.datePicker date];
+    NSLog(@"Saving %@ date", receipt.timeStamp);
+    
     receipt.note = self.noteField.text;
     NSLog(@"Saving %@ description", receipt.note);
     NSLog(@"receipt %@", receipt.description);
-
+    
+    [receipt addTag:self.tags];
+    NSLog(@"Number of tags for this receipt %lu", (unsigned long)self.tags.count);
+    
     [appDelegate saveContext];
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    if (!self.tags) {
+    self.tags = [NSMutableSet new];
+    }
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+
 
 /*
 #pragma mark - Navigation
